6b95ccb9d3818f012874552dcbc5bb35da8c1a54
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-06-02 23:09:02 -0300

    Add ESM support

---
+++ dist/bundle.es.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+var StyledButton = styled.button.withConfig({
+  displayName: "Button__StyledButton",
+  componentId: "cy85s3-0"
+})(["border:2px solid #131313;padding:8px 10px;background:#fafafa;border-radius:3px;"]);
+
+var Button = function Button(_ref) {
+  var children = _ref.children,
+      props = _objectWithoutProperties(_ref, ["children"]);
+
+  return React.createElement(StyledButton, props, children);
+};
+
+var index = styled.h1.withConfig({
+  displayName: "H1",
+  componentId: "sc-12qsmf2-0"
+})(["font-size:4em;"]);
+
+var index$1 = styled.h2.withConfig({
+  displayName: "H2",
+  componentId: "k3byfw-0"
+})(["font-size:2em;"]);
+
+var index$2 = styled.h3.withConfig({
+  displayName: "H3",
+  componentId: "lzm212-0"
+})(["font-size:1.2em;"]);
+
+export { Button, index as H1, index$1 as H2, index$2 as H3 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
+import pkg from './package.json';
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
   external: [
     'react',
     'styled-components'
   ],
   plugins: [
     resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    })
+    babel({ exclude: 'node_modules/**' })
   ]
 };
