940bbfaea30b406cf8b8b6f2509931649da3d35e
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-06-06 10:37:46 -0300

    New structure with entrypoints on top level and multiple package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,46 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
+const baseConfig = {
+  external: [
+    'react',
+    'styled-components'
+  ],
+  plugins: [
+    resolve(),
+    babel({ exclude: 'node_modules/**' })
+  ]
+}
+
 export default [
   {
-    input: {
-      index: 'src/index.js',
-      components: 'src/components/index.js',
-      colors: 'src/colors/index.js',
-    },
+    ...baseConfig,
+    input: './src/components/index.js',
     output: [
       {
-        dir: 'dist',
+        // exports: 'named',
+        file: 'components/dist/react-component-lib-components.cjs.js',
         format: 'cjs'
       },
       {
-        dir: 'dist',
+        file: 'components/dist/react-component-lib-components.esm.js',
         format: 'esm',
-        entryFileNames: '[name].mjs'
       }
-    ],
-    external: [
-      'react',
-      'styled-components'
-    ],
-    plugins: [
-      resolve(),
-      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  {
+    ...baseConfig,
+    input: './src/colors/index.js',
+    output: [
+      {
+        // exports: 'named',
+        file: 'colors/dist/react-component-lib-colors.cjs.js',
+        format: 'cjs'
+      },
+      {
+        file: 'colors/dist/react-component-lib-colors.esm.js',
+        format: 'esm',
+      }
     ]
   },
 ];
